6df02b27751ade874834167deea598932bb7b9a8
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-08-21 22:18:20 -0700

    Got the cow to print out but he looks like he just came thorugh a blender

---
+++ lab-eddie/src/main.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './style/main.scss';
+import cowsay from 'cowsay-browser'
+import faker from 'faker'
+
+class CowBody extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cow: ''
+    }
+    this.newIpsum = this.newIpsum.bind(this);
+  }
+
+  newIpsum(e) {
+    let ipsum = faker.lorem.sentence();
+
+    this.setState(state => {
+      return {
+        cow: cowsay.think({
+          text: ipsum
+        })
+      }
+    })
+  }
+
+  render() {
+    return(
+      <div className="test">
+        <button onClick={this.newIpsum}>'Please Click Meeeeeee'</button>
+        <p>{this.state.cow}</p>
+      </div>
+    )
+  }
+
+
+}
+
+class CowHead extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <header className='header'>
+        <h1>Gerate Cowsay Lorem</h1>
+      </header>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <CowHead />
+        <CowBody />
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+ReactDom.render(<App />, document.getElementById('root'));
--- lab-eddie/webpack.config.js
+++ lab-eddie/webpack.config.js
@@ -6,9 +6,9 @@ const ExtractPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   devtool: 'cheap=module-eval-source-map',
   entry: `${__dirname}/src/main.js`,
-  ouput: {
+  output: {
     path: `${__dirname}/build`,
-    pathPublic: '/',
+    publicPath: '/',
     filename: 'bundle-[hash].js'
   },
   plugins: [

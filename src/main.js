887233c58c25c03e3bb01ae49479bc19274b0bf5
Bret Ldenburg <bret.ladenburg@gmail.com>
2017-08-22 18:19:56 -0700

    made some changes to the lab

--- src/main.js
+++ src/main.js
@@ -4,10 +4,44 @@ import ReactDom from 'react-dom';
 import cowsay from 'cowsay-browser';
 import faker from 'faker';
 
-class Navbar extends React.Component {
+class CowBody extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      cow: ''
+    }
+    this.ipsum = this.ipsum.bind(this);
   };
+
+  ipsum(e) {
+    let nonsense = faker.lorem.words();
+
+    this.setState(state => {
+      return {
+        cow: cowsay.say({
+          text: nonsense
+        })
+      }
+    })
+  }
+
+  render() {
+    return(
+      <div className="test">
+        <button onClick={this.ipsum}>'Click Me'</button>
+        <pre>{this.state.cow}</pre>
+      </div>
+    )
+  }
+
+
+};
+
+class CowHead extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
@@ -16,34 +50,18 @@ class Navbar extends React.Component {
       </header>
     );
   };
-};
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      content: ''
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
-    console.log('clicking');
-    this.setState(
-      {
-        content: cowsay.say({
-          text: faker.lorem.words(10)
-        })
-      }
-    )
   }
 
   render() {
     return (
       <div>
-        <Navbar />
-        <p onClick={this.handleClick}>Content: {this.state.content}</p>
+        <CowHead />
+        <CowBody />
       </div>
     )
   }
